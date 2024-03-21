2b5d0d7e59fd73f4b45d92b69bddbbca53f454f7
Breno Papa <bpapa@outlook.com.br>
2024-03-21 09:05:02 -0300

    new method

--- carol-bq-query.py
+++ carol-bq-query.py
@@ -1,6 +1,6 @@
-from pycarol import Carol, PwdAuth
-from google.cloud import bigquery
-from google.oauth2 import service_account
+from pycarol import Carol, PwdAuth, BQ
+# from google.cloud import bigquery
+# from google.oauth2 import service_account
 from dotenv import load_dotenv
 import json, os
 
@@ -9,22 +9,19 @@ load_dotenv(".env")
 CAROLUSER = os.environ.get("CAROLUSER")
 CAROLPWD = os.environ.get("CAROLPWD")
 CAROLTENANT = os.environ.get("CAROLTENANT")
+CAROLAPP = os.environ.get("CAROLAPP")
 CAROLORGANIZATION = os.environ.get("CAROLORGANIZATION")
 CAROLCONNECTORID= os.environ.get("CAROLCONNECTORID")
 CAROLSA = json.loads(os.environ.get("CAROLSA"))
 
 sql_query = '''
-select * from shd_Protheus_Workstations limit 10
+select * from shd_Protheus_workstations limit 10
 '''
 
-carol = Carol(auth=PwdAuth(CAROLUSER, CAROLPWD), organization=CAROLORGANIZATION, domain=CAROLTENANT, connector_id=CAROLCONNECTORID)
+carol = Carol(auth=PwdAuth(CAROLUSER, CAROLPWD), organization=CAROLORGANIZATION, domain=CAROLTENANT, connector_id=CAROLCONNECTORID, app_name=CAROLAPP)
 
-credentials = service_account.Credentials.from_service_account_info(CAROLSA)
-client = bigquery.Client(credentials=credentials)
-config = bigquery.QueryJobConfig(priority="BATCH", default_dataset=f"{CAROLSA['project_id']}.{carol._current_env()['mdmId']}")
 try:
-        query_job = client.query(sql_query, job_config=config, )
-        df = query_job.result().to_dataframe(create_bqstorage_client=True)
-        df.to_csv("result.csv", index=False)
+        result = BQ(carol).query(sql_query)
+        result.to_csv("result.csv", index=False)
 except Exception as e:
         print("error: {}".format(e))
